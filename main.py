b428fa567f7a253f5bad7a50b01cc8937507121b
adityak74 <akarnam37@gmail.com>
2022-06-05 16:34:04 -0500

    chore: add logging

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import multiprocessing
 import time
-
+import logging
 import dotenv
 from fastapi import FastAPI, Request, Response
 
 from src.token_bucket import TokenBucket, refresh_tokens
 
+logging.basicConfig(format="%(asctime)s\t%(message)s",filename='token-bucket.log', encoding='utf-8', level=logging.DEBUG)
 dotenv.load_dotenv()
 app = FastAPI()
 token_bucket = TokenBucket()
@@ -15,7 +16,9 @@ multiprocessing.Process(target=refresh_tokens).start()
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
-    if token_bucket.get_token_count() < 1:
+    current_token_count = token_bucket.get_token_count()
+    logging.debug(f"{current_token_count}")
+    if current_token_count < 1:
         return Response(None, status_code=503)
     # remove a token from bucket
     token_bucket.reduce_token()
