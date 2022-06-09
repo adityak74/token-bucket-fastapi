ceb8d7a886b68498df296edffe6a5588ed2dfb37
adityak74 <akarnam37@gmail.com>
2022-06-09 00:30:11 -0500

    chore: cleanup sample header logic

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ multiprocessing.Process(target=refresh_tokens).start()
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
     current_token_count = token_bucket.get_token_count()
     logging.debug(f"{current_token_count}")
     if current_token_count < 1:
@@ -23,8 +22,6 @@ async def add_process_time_header(request: Request, call_next):
     # remove a token from bucket
     token_bucket.reduce_token()
     response = await call_next(request)
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
     return response
 
 
